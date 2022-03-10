7ef168d4a23e28c3070d0ecaa25d0abea2a6e444
Francisco V. Diaz <fevarga1@uc.cl>
2022-03-10 00:29:18 -0300

    added logo, created subtemplates. hito 2 done!

--- web/views.py
+++ web/views.py
@@ -5,14 +5,14 @@ from django.template import loader
 
 
 def index(request):
-    context = {'titulo': 'HOME', 'contenido' : 'indice'}
+    context = {'contenido' : 'indice'}
     return render(request, 'base.html', context)
 
 def about(request):
-    context = {'titulo': 'ABOUT', 'contenido' : 'acerca'}
+    context = {'contenido' : 'acerca'}
     return render(request, 'base.html', context)
 
 def welcome(request):
-    context = {'titulo': 'WELCOME', 'contenido' : 'bienvenido cliente'}
+    context = {'contenido' : 'bienvenido cliente'}
     return render(request, 'base.html', context)
 
